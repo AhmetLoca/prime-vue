49ef67053cb202c63e39f33d486b72e680f3c687
ahmet <locaahmet@gmail.com>
2022-12-12 19:34:51 +0300

    primevue

---
+++ prime-vue-setup/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+const text = ref();
+</script>
+
+<template>
+  <h1>Welcome to PrimeTime</h1>
+  
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="txt" type="text" v-model="text" />
+      <label for="txt">Username</label>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
